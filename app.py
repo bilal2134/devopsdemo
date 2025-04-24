63ac591cefbf7d78a937203161c4d8111f66d767
bilal2134 <i210575@nu.edu.pk>
2025-04-24 10:38:38 +0500

    fixed workflow

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return "Hello, DevOps World!"
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-    #test
