c9448631dbe6b066995296957ca221e2ff86c47e
bilal2134 <i210575@nu.edu.pk>
2025-04-24 19:05:43 +0500

    Add ML training, DVC, and MLflow experiment tracking

---
+++ train.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import joblib
+import mlflow
+import mlflow.sklearn
+
+df = pd.read_csv('data.csv')
+X = df.drop('target', axis=1)
+y = df['target']
+
+with mlflow.start_run():
+    model = LogisticRegression()
+    model.fit(X, y)
+    mlflow.sklearn.log_model(model, "model")
+    mlflow.log_param("solver", model.solver)
+    mlflow.log_metric("score", model.score(X, y))
+    joblib.dump(model, 'model.joblib')
