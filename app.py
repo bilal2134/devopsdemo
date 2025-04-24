1b8ff66570e5f2fe1f326eaeae747e4422db139d
bilal2134 <i210575@nu.edu.pk>
2025-04-24 18:55:00 +0500

    added kubernetes

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-
+metrics = PrometheusMetrics(app)
 
 @app.route("/")
 def home():
     return "Hello, DevOps World!"
-
 
 if __name__ == "__main__":
     app.run(debug=True)
